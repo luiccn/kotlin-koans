7cf0234078ad15b58a64102bfb2729a4867e9802
ciafrino <luiz.ciafrino@gmail.com>
2016-12-03 20:38:14 -0200

    forLoop

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,16 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int =
+            when {
+                year != other.year -> year - other.year
+                month != other.month -> month - other.month
+                else -> dayOfMonth - other.dayOfMonth
+            }
+}
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
+
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +18,18 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(val start: MyDate, val endInclusive: MyDate) : Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> {
+        val current = this.start
+        val end = this.endInclusive
+        return object : Iterator<MyDate> {
+            override fun hasNext(): Boolean = current <= end
+            override fun next(): MyDate = current.nextDay()
+        }
+    }
+}
+
+operator fun DateRange.contains(other: MyDate) : Boolean = this.start < other && this.endInclusive > other
+
+
+
